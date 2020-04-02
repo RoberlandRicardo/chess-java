eaedd78d2eb334d472e7cb5527c98168a2e89cd7
Gravenai <etadoninguem@gmail.com>
2020-04-01 21:40:59 -0300

    Primeiro commit

---
+++ src/board/Position.java
@@ -0,0 +1,33 @@
+package board;
+
+public class Position {
+
+	private Integer row;
+	private Integer col;
+	
+	public Position (int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public Integer getColumn() {
+		return col;
+	}
+
+	public void setColumn(Integer col) {
+		this.col = col;
+	}
+	
+	@Override
+	public String toString() {
+		return row + "/" + col;
+	}
+}
---
+++ src/program/Program.java
@@ -0,0 +1,14 @@
+package program;
+
+import board.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+	Position pos = new Position(3,5);
+	System.out.println(pos);
+	
+	}
+
+}
