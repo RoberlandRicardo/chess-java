ebe7c62b05155ebcc81bc46281cd9da52bd7e19c
Gravenai <etadoninguem@gmail.com>
2020-04-07 23:54:26 -0300

    Catch Exceptions inputMismatch and ChessException

--- src/program/Program.java
+++ src/program/Program.java
@@ -1,8 +1,10 @@
 package program;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import board.Piece;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPosition;
 
@@ -14,16 +16,25 @@ public class Program {
 		ChessMatch match = new ChessMatch();
 		
 		while (true) {
-			Interface.printBoard(match.getPieces());
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = Interface.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = Interface.readChessPosition(sc);
-			
-			Piece capturedPiece = match.chessMove(source, target);
+			try {
+				Interface.clearScreen();
+				Interface.printBoard(match.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = Interface.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = Interface.readChessPosition(sc);
+				
+				Piece capturedPiece = match.chessMove(source, target);
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 
